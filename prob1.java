37a29bf66a6be9f8736b2cde8fe72d2a4f3aec8f
shivangiS04 <shivangimasterblaster@gmail.com>
2025-02-07 19:49:08 +0530

    git commit

--- prob1.java
+++ prob1.java
@@ -1,23 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
+/ Time Complexity : O(N)
+// Space Complexity : O(H)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         // TC = O(N* logN)
+//         // SC = O(H)
+//         // null
+//         if(root == null) return true;
+//         if(Math.abs(height(root.left) - height(root.right)) > 1) return false;
+//         return isBalanced(root.left) && isBalanced(root.right);
+//     }
+//     private int height(TreeNode root) {
+//         // base
+//         if(root == null) return 0;
+//         // logic
+//         return Math.max(height(root.left), height(root.right)) + 1;
+//     }
+// }
+
+// Optimized - As we are calling height method at every node, we will stop calling height if at any part of subtree, unbalanced is detected & from here we return -1
 
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-
-        for (int rowNum = 0; rowNum < numRows; rowNum++) {
-            List<Integer> row = new ArrayList<>();
-            for (int i = 0; i <= rowNum; i++) {
-                row.add(1); // Initialize with 1
-            }
-
-            for (int j = 1; j < row.size() - 1; j++) {
-                row.set(j, triangle.get(rowNum - 1).get(j - 1) + triangle.get(rowNum - 1).get(j));
-            }
-
-            triangle.add(row);
-        }
-
-        return triangle;
+    public boolean isBalanced(TreeNode root) {
+        // TC = O(N) (if we are finding imbalance returning -1 from there on), SC = O(H)
+        // null
+        if(root == null) return true;
+        return height(root) != -1;
+    }
+    private int height(TreeNode root) {
+        // base
+        if(root == null) return 0;
+        // logic
+        int left = height(root.left);
+        int right = height(root.right);
+        if(Math.abs(left - right) > 1) return -1;
+        if(left == -1 || right == -1) return -1; // checking if any part is unbalanced, return -1 instead of calling height for that node
+        return Math.max(left, right) + 1; // actual height
     }
 }
--- prob2.java
+++ prob2.java
@@ -1,26 +1,51 @@
-import java.util.Arrays;
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public int findPairs(int[] nums, int k) {
-        Arrays.sort(nums);
-        int left = 0;
-        int right = 1;
-        int result = 0;
-
-        while (left < nums.length && right < nums.length) {
-            if (left == right || nums[right] - nums[left] < k) {
-                right++;
-            } else if (nums[right] - nums[left] > k) {
-                left++;
-            } else {
-                left++;
-                result++;
-                while (left < nums.length && nums[left] == nums[left - 1]) {
-                    left++;
-                }
-            }
+    public boolean isPalindrome(ListNode head) {
+        // TC = O(N), SC = O(1), reverse 2nd half, compare 1st & 2nd half
+        // null
+        if(head == null || head.next == null) return true;
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null) { // reach mid
+            slow = slow.next;
+            fast = fast.next.next;
         }
-
-        return result;
+        fast = reverse(slow.next); // reversing the 2nd half
+        slow.next = null; // dividing the LL into 2
+        slow = head; // taking slow to the start
+        while(fast != null) { 
+            if(slow.val != fast.val) return false; // checking the vals
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode prev = null, curr = head, fast = head.next;
+        while (fast != null) {
+            curr.next = prev;
+            prev = curr;
+            curr = fast;
+            fast = fast.next;
+        }
+        curr.next = prev;
+        return curr;
     }
 }
